cb0fd6ab6697378f58d939c93306082403db37c1
hyuckkim <monkeyhyuck@gmail.com>
2023-12-11 21:18:59 +0900

    init

---
+++ index.ts
@@ -0,0 +1,68 @@
+export function separate(text: string, regexp: RegExp): (string | RegExpExecArray)[] {
+  const slice = text.split(regexp);
+  const reg = getAllRegexp(text, regexp);
+  const texts = takeOnlyText(slice, reg);
+
+  return texts.reduce((prev: (string | RegExpExecArray)[], curr, idx) => {
+    if (idx < reg.length) return [...prev, curr, reg[idx]];
+    else return [...prev, curr] 
+  }, []);
+}
+
+export function getAllRegexp(text: string, regexp: RegExp): RegExpExecArray[] { 
+  if (!regexp.global) throw new Error(`Regexp-misc getAllRegexp: regexp ${regexp} must be global`);
+  const result: RegExpExecArray[] = [];
+
+  let m: RegExpExecArray | null = null;
+  do {
+    m = regexp.exec(text);
+    if (!!m) result.push(m);
+  } while(m);
+
+  return result;
+}
+
+function takeOnlyText(split: string[], array: RegExpExecArray[]): string[] {
+  const result: string[] = [];
+  let s = 0;
+  for (let i = 0; (i + s) < split.length; i++) {
+    result.push(split[i + s]);
+    if (!!array[i]) s += array[i].length - 1;
+  }
+
+  return result;
+}
+
+export function findAndReplace(text: string, regexp: RegExp, replace: string): string {
+  const d = separate(text, regexp);
+
+  return d.reduce((prev: string, curr) => {
+    if (typeof curr === "string") {
+      return prev + curr;
+    } 
+    else {
+      return prev + replaceByRegexp(replace, curr);
+    }
+  }, "");
+}
+function replaceByRegexp(text: string, regexp: RegExpExecArray): string {
+  const d = separate(text, /\$([0-9]+)/g);
+
+  return d.reduce((prev: string, curr) => {
+    if (typeof curr === "string") {
+      return prev + curr;
+    }
+    else {
+      const no = Number.parseInt(curr[1]);
+      return prev + regexp[no];
+    }
+  }, "");
+}
+
+export function match<T>(text: string, array: [RegExp, (found: RegExpExecArray) => T][]): T | null {
+  for (const [regexp, func] of array) {
+    const m: RegExpExecArray | null = regexp.exec(text);
+    if (!!m) return func(m);
+  }
+  return null;
+}
---
+++ readme.md
@@ -0,0 +1,60 @@
+# regexp-misc
+my small tool for Regular Expression.
+
+
+## usage
+
+### `separate(text: string, regexp: RegExp): (string | RegExpExecArray)[]`
+Finds all regular expressions in the text, then mixes them with results that do not contain regular expressions.
+
+Therefore, the return value is an odd-numbered array, and plain `string` and `RegExpExecArray` appear alternately.
+
+In particular, it does not remove leading and trailing empty strings. If necessary, you will need to remove it manually.
+
+```typescript
+const s = separate('hello, <h>regexp</h> <b>world</b>!', /<.+?>(.+?)<\/.+?>/g);
+console.log(s);
+```
+```
+[
+  'hello, ',
+  (RegExpExecArray),
+  ' ',
+  (RegExpExecArray),
+  '!'
+]
+```
+
+### `findAndReplace(text: string, regexp: RegExp, replace: string): string`
+Perform find and replace like vscode does.
+
+```typescript
+const f = findAndReplace('hello, <h>regexp</h> <b>world</b>!', /<.+?>(.+?)<\/.+?>/g, "'$1'");
+console.log(f);
+```
+```
+hello, 'regexp' 'world'!
+```
+
+### `match<T>(text: string, array: [RegExp, (found: RegExpExecArray) => T][]): T | null`
+In an array of regular expressions, executes the function associated with the first matching regular expression.
+
+You can specify the return value of a function, and then return value's type will be specified.
+
+```typescript
+const m = match('031-123-4567', [
+  [/010-[0-9]{4}-[0-9]{4}/, exp => {
+    return 'mobile ' + exp[0];
+  }],
+  [/02-([0-9]{3}-[0-9]{4})/, exp => {
+    return 'seoul ' + exp[1];
+  }],
+  [/031-([0-9]{3}-[0-9]{4})/, exp => {
+    return 'gyeonggi ' + exp[1];
+  }],
+]);
+console.log(m);
+```
+```
+gyeonggi 123-4567
+```
